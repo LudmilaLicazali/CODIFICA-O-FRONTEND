8053000dea7e0b775293ff25f6c847348197572a
Ludmila <ludmilalicazali@gmail.com>
2024-03-18 23:26:36 -0300

    Testes na Atividade #12

---
+++ Atividade #12/scripit.js
@@ -0,0 +1,28 @@
+// function altFont(){
+//     var texto = document.querySelector("#texto");
+//     var tamanhoAtual = 
+    
+// }
+
+
+// function altCorFont(){
+//    var paragrafo = cor
+//     paragrafo[1].style.color= "blue"
+// }
+
+function insereTexto(){
+   var novoTexto = "Novo Paragrafo";
+   var paragrafo = texto.querySelector('paragrafo')
+   document.querySelector("texto").texto += novoTexto;
+}
+
+
+
+
+
+// let insere = document.querySelector('.insere')
+// insere.addEventListener('click', () => {
+//     insereTexto();
+// });
+
+
